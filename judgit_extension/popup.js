2bb1c7d182c752fa75cc124612df63502491ed95
Antoine <gjeloshaj.antoine@gmail.com>
2019-08-04 15:27:47 +0200

    get title in popup

---
+++ judgit_extension/popup.js
@@ -0,0 +1,12 @@
+
+// chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+//     let tab = tabs[0]
+//     document.getElementById('url').innerText = tab.url
+// })
+
+let currentTab = new Promise((resolve,reject)=>chrome.tabs.query({ active: true, currentWindow: true }, resolve))
+currentTab.then(tabs=>{
+    console.log(tabs[0]);
+    tabs[0].title
+    document.getElementById('url').innerText = tabs[0].url
+})
