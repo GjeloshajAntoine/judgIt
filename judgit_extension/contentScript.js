d5721c759c3e662c7883d84c78ba4fc2e7b6fcc5
Antoine <gjeloshaj.antoine@gmail.com>
2019-08-07 00:18:23 +0200

    consume api for total colors gradient

--- judgit_extension/contentScript.js
+++ judgit_extension/contentScript.js
@@ -1,13 +1,26 @@
-console.log('contentscipt#');
+console.log('contentscipt_');
 
 //console.log(document.querySelectorAll('a'));
 
 document.querySelectorAll('a').forEach(e=> {
-    fetch('localhost:3000/votes/colorTotal/'+e.href)
+    console.log('before fetch');
+    let url = e.href
+    if (!url) return ;
+    fetch('http://localhost:3000/votes/colorTotal',
+     {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({url: url})
+      }
+    )
     .then(res=>{
-        return res.json()
+        return res.json().catch(console.log)
     })
     .then(colors=>{
+        if(colors.hasOwnProperty('url')) console.log(colors)
         e.addEventListener('mouseover', function () {
             this.style.backgroundImage = colorLineGraph(colors.green,colors.yellow,colors.red)
             // this.style.backgroundImage = 'linear-gradient(transparent, transparent),linear-gradient(transparent, transparent),linear-gradient(to right, #30e8bf, #F90F00)'
