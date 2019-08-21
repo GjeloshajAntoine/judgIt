e7efec733ff2c4a836a59a37af2ee8d49cad2d88
Antoine <gjeloshaj.antoine@gmail.com>
2019-08-22 01:52:12 +0200

    query moved to model

--- judgit_server/users/middlerware.js
+++ judgit_server/users/middlerware.js
@@ -1,16 +1,18 @@
-module.exports = function (db)  {
+const userModelInit = require('./model')
+module.exports = async function (db)  {
+    const userModel = userModelInit(db)
     return {
         connected : function (req,res, next)  {
-            let token = ''
-            db.query('SELECT id FROM user WHERE extension_token = $1',[token])
-            .then(data =>{
-                if (data.rows.length) {
+            let token = req.cookies 
+            userModel.getUserIdByExtensionToken(token)
+            .then(userId=>{
+                if (userId) {
                     res.locals.user = {id: data.rows[0].id}
                     next()
                 } else {
                     res.json({error:true,msg:'not connected'})
                 }
             })
-        }
+
     }
 }
