324d3a10a3777a17df13bea1a14ba46e7c866c74
Antoine <gjeloshaj.antoine@gmail.com>
2019-08-22 01:52:21 +0200

    user model

---
+++ judgit_server/users/model.js
@@ -0,0 +1,16 @@
+module.exports = function (db) {
+    return {
+        createUserWithExtensionToken : function (token) {
+            return db.query('INSERT INTO users(extension_token) VALUES($1) RETURNING id',
+            [token]).then(data=>{
+                return data.rows[0].id
+            })
+        },
+        getUserIdByExtensionToken : function (token) {
+            return db.query('SELECT id FROM users WHERE extension_token = $1',
+            [token]).then(token=>{
+                return data.rows.length ? data.rows[0].id : false  
+            })
+        }
+    }
+}
