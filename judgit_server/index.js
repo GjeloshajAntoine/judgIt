bda9e1416cde9a94d50f1008af530afc7f60a7d6
Antoine <gjeloshaj.antoine@gmail.com>
2019-08-22 01:51:16 +0200

    added cookie parser and uuid

--- judgit_server/index.js
+++ judgit_server/index.js
@@ -7,15 +7,24 @@ const client = new Client(config.postgres)
 const users = require('./users/routes')
 const usersMiddelware = require('./users/middlerware')
 const votes = require('./votes/routes')
+const cookieParser = require('cookie-parser')
+
 
 client.connect().catch(console.log)
 
 app.use(cors())
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(cookieParser())
 app.use('/users', users(client))
 app.use('/votes', votes(client, usersMiddelware(client)))
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 })
+
+
+
+function logInternalError(req,res,next) {
+    
+}
