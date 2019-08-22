2064b85525a1e94e9bfba662d9c7b1a2936a55f4
Antoine <gjeloshaj.antoine@gmail.com>
2019-08-22 20:42:44 +0200

    Optional connection middleware

--- judgit_server/users/middlerware.js
+++ judgit_server/users/middlerware.js
@@ -1,17 +1,25 @@
 const userModelInit = require('./model')
-module.exports = async function (db)  {
+module.exports =  function (db)  {
     const userModel = userModelInit(db)
     return {
         connected : function (req,res, next)  {
-            let token = req.cookies 
+            let token = req.cookies.token_user_extension
             userModel.getUserIdByExtensionToken(token)
             .then(userId=>{
                 if (userId) {
-                    res.locals.user = {id: data.rows[0].id}
+                    res.locals.user = {id: userId}
                     next()
                 } else {
                     res.json({error:true,msg:'not connected'})
                 }
+            })
+        },
+        connectedOptional : function (req,res, next) {
+            let token = req.cookies.token_user_extension
+            userModel.getUserIdByExtensionToken(token)
+            .then(userId=>{
+                res.locals.user = {id: userId}
+                next()
             })
         }
     }
