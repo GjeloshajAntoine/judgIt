d3d1bba88e401bd69da4db682b02989ec333e49b
Antoine <gjeloshaj.antoine@gmail.com>
2019-08-24 17:18:01 +0200

    format  + text autocompletion

--- judgit_server/votes/model.js
+++ judgit_server/votes/model.js
@@ -1,9 +1,9 @@
 module.exports = function (db) {
     return {
-        getLinkId : function (url) {
-            return db.query('SELECT id FROM links WHERE url = $1',[url]).then(data=>data.rows.length?data.rows[0]:{id:0}).catch(console.error)
+        getLinkId: function (url) {
+            return db.query('SELECT id FROM links WHERE url = $1', [url]).then(data => data.rows.length ? data.rows[0] : { id: 0 }).catch(console.error)
         },
-        getVotes : function (linkId, userId) {
+        getVotes: function (linkId, userId) {
             return db.query(`SELECT text_id,vt.text,color,SUM(CASE WHEN user_id = $1 THEN 1 ELSE 0 END) as is_upvoted_by_current_user ,count(*)as nbr,((COUNT(*) / SUM(COUNT(*)) OVER ())*100) AS percentage
             FROM votes
             RIGHT JOIN links ON links.id = votes.link_id
@@ -11,48 +11,54 @@ module.exports = function (db) {
             WHERE links.id = $2
             GROUP BY text_id,color,vt.text      
             ORDER BY nbr DESC
-            `,[userId,linkId] ).then(data =>{
-                let parsedRows = data.rows.map(row=>{
+            `, [userId, linkId]).then(data => {
+                let parsedRows = data.rows.map(row => {
                     row.is_upvoted_by_current_user = parseInt(row.is_upvoted_by_current_user)
-                    row.percentage = parseFloat(row.percentage).toFixed(2)
+                    row.percentage = parseFloat(row.percentage).toFixed(0)
                     return row
                 })
                 return parsedRows;
             })
         },
-        getColorTotalUrl : function (link) {
+        getColorTotalUrl: function (link) {
             let res = db.query('SELECT * FROM links WHERE url = $1', [link])
-            return res.then(data =>{
-                console.log('link from db :',data.rows.length?data.rows[0]:'');
-                return data.rows.length?data.rows[0]:{}
+            return res.then(data => {
+                console.log('link from db :', data.rows.length ? data.rows[0] : '');
+                return data.rows.length ? data.rows[0] : {}
             }).catch(console.error);
         },
-        upVote : function (userId,linkId,text_id,color) {
+        upVote: function (userId, linkId, text_id, color) {
             return db.query('INSERT INTO votes(user_id,link_id,text_id,color) VALUES($1,$2,$3,$4)',
-            [userId,linkId,text_id,color]).then(data=>{
-                return data.rows
-            })
+                [userId, linkId, text_id, color]).then(data => {
+                    return data.rows
+                })
         },
-        unVote : function (userId,linkId,textId,color) {
+        unVote: function (userId, linkId, textId, color) {
             return db.query('DELETE FROM votes WHERE user_id = $1 AND link_id = $2 AND text_id = $3 AND color = $4',
-            [userId,linkId,textId,color]).then(data=>{
-                return data.rows
-            })
+                [userId, linkId, textId, color]).then(data => {
+                    return data.rows
+                })
         },
-        CreateOrUpVoteLinkId : function (linkId,userId,VoteText,VoteColor) {
+        CreateOrUpVoteLinkId: function (linkId, userId, VoteText, VoteColor) {
             //"SELECT CreateOrUpVoteLinkId(1,1,'other new comment','')"
-            return  db.query('SELECT  CreateOrUpVoteLinkId($1,$2,$3,$4::colors)', 
-                [userId,linkId,VoteText,VoteColor]).then(data=>{
-                    return data.rows.length?data.rows:[]
+            return db.query('SELECT  CreateOrUpVoteLinkId($1,$2,$3,$4::colors)',
+                [userId, linkId, VoteText, VoteColor]).then(data => {
+                    return data.rows.length ? data.rows : []
                 }).catch(console.warn)
         },
-        CreateOrUpVoteLinkUrl : function (linkUrl,userId,VoteText,VoteColor) {
+        CreateOrUpVoteLinkUrl: function (linkUrl, userId, VoteText, VoteColor) {
             //"SELECT CreateOrUpVoteLinkUrl(1,'http://','other new comment','')"
             return db.query('SELECT createorupvotelinkurl($1,$2,$3,$4::colors)',
-            [userId,linkUrl,VoteText,VoteColor]
-            ).then(data=>{
-                return data.rows.length?data.rows:[]
+                [userId, linkUrl, VoteText, VoteColor]
+            ).then(data => {
+                return data.rows.length ? data.rows : []
             }).catch(console.error)
+        },
+        getTextLike : function (text,limit) {   
+            let formatedText = '%' + text + '%'
+            return db.query('SELECT text FROM votetexts WHERE text LIKE $1 LIMIT $2',
+                [formatedText,n]
+            ).then(data=>data.rows)
         }
 
     }
--- judgit_server/votes/routes.js
+++ judgit_server/votes/routes.js
@@ -3,62 +3,68 @@ const voteModelUnInit = require('./model')
 
 module.exports = function (db, usersMiddelware) {
     const voteModel = voteModelUnInit(db)
-    router.post('/linkId',(req,res)=>{
-        voteModel.getLinkId(req.body.url).then(data=>{
+    router.post('/linkId', (req, res) => {
+        voteModel.getLinkId(req.body.url).then(data => {
             console.warn(data);
-            
+
             res.json(data)
         }).catch(console.warn)
     })
 
-    router.post('/votes',usersMiddelware.connectedOptional,(req,res)=>{
-        let {linkId} = req.body
-        voteModel.getVotes(linkId,res.locals.user.id)
-        .then(res.json.bind(res))
+    router.post('/votes', usersMiddelware.connectedOptional, (req, res) => {
+        let { linkId } = req.body
+        voteModel.getVotes(linkId, res.locals.user.id)
+            .then(res.json.bind(res))
     })
 
-    router.post('/colorTotal',(req,res)=>{
-        console.log('requested url',req.body.url); 
+    router.post('/colorTotal', (req, res) => {
+        console.log('requested url', req.body.url);
         voteModel.getColorTotalUrl(req.body.url)
-        .then(data=>{
-            console.log('for url',req.body.url);   
-            console.log('res data :',data)
-            res.json(data)
-        })
+            .then(data => {
+                console.log('for url', req.body.url);
+                console.log('res data :', data)
+                res.json(data)
+            })
     })
 
-    router.post('/colorTotalBulk',(req,res)=>{
-        let {urls} = req.body
-        let urlsAndColorsProms = urls.map(url=>{
-            return voteModel.getColorTotalUrl(url).then(colors=>{
+    router.post('/colorTotalBulk', (req, res) => {
+        let { urls } = req.body
+        let urlsAndColorsProms = urls.map(url => {
+            return voteModel.getColorTotalUrl(url).then(colors => {
                 colors.url = url // add colors to the totals so we can associate
                 return colors
             })
         })
         Promise.all(urlsAndColorsProms)
-        .then(urlsAndColors=>res.json(urlsAndColors))
+            .then(urlsAndColors => res.json(urlsAndColors))
     })
 
-    router.post('/upVote',usersMiddelware.connected,(req,res)=>{
-        let {linkId,textId,color} = req.body
+    router.post('/upVote', usersMiddelware.connected, (req, res) => {
+        let { linkId, textId, color } = req.body
         let userId = res.locals.user.id
-        voteModel.upVote(userId,linkId,textId,color).then(o=>res.json(o))
+        voteModel.upVote(userId, linkId, textId, color).then(o => res.json(o))
     })
 
-    router.post('/unVote',usersMiddelware.connected,(req,res)=>{
-        let {linkId,textId,color} = req.body
-        voteModel.unVote(res.locals.user.id,linkId,textId,color).then(o=>res.json(o))
+    router.post('/unVote', usersMiddelware.connected, (req, res) => {
+        let { linkId, textId, color } = req.body
+        voteModel.unVote(res.locals.user.id, linkId, textId, color).then(o => res.json(o))
     })
 
-    router.post('/CreateOrUpVoteLinkId',usersMiddelware.connected,(req,res)=>{
-        let {linkId,text,color} = req.body
+    router.post('/CreateOrUpVoteLinkId', usersMiddelware.connected, (req, res) => {
+        let { linkId, text, color } = req.body
         let userId = res.locals.user.id
-        voteModel.CreateOrUpVoteLinkId(linkId,userId,text,color).then(res.json.bind(res))
+        voteModel.CreateOrUpVoteLinkId(linkId, userId, text, color).then(res.json.bind(res))
     })
-    router.post('/CreateOrUpVoteLinkUrl',usersMiddelware.connected,(req,res)=>{
-        let {url,text,color} = req.body
+    router.post('/CreateOrUpVoteLinkUrl', usersMiddelware.connected, (req, res) => {
+        let { url, text, color } = req.body
         let userId = res.locals.user.id
-        voteModel.CreateOrUpVoteLinkUrl(url,userId,text,color).then(res.json.bind(res))
+        voteModel.CreateOrUpVoteLinkUrl(url, userId, text, color).then(res.json.bind(res))
+    })
+
+    router.get('/autocompleteText/:text/:n?',(req,res)=>{
+        let {text,n} = req.params.text
+        n = n? n : 10
+        voteModel.getTextLike(text,n).then(data=>res.json(data))
     })
 
     return router
