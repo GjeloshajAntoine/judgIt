5a19744fe6184fbcbcb566f32cbf380c3dc55bef
Antoine <gjeloshaj.antoine@gmail.com>
2019-09-07 17:57:40 +0200

    githook support

--- judgit_server/admin/routes.js
+++ judgit_server/admin/routes.js
@@ -1,10 +1,20 @@
 const router = require('express').Router();
 const path = require('path');
+const { exec } = require('child_process');
 
 module.exports = function () {
 
     router.post('/git/hooks/push',(res,req)=>{
-
+        console.log('received push from github');
+        let mainFolder = path.dirname(require.main.filename)
+        exec('git pull origin master',{pwd:mainFolder},(err=>{
+            if (err) {
+                console.error('Error in git push hook ,pull command returned:')
+                console.error(err)
+            } else {
+                console.log('git pull looks okay')        
+            }
+        }))
     })
     router.get('/version',(req,res)=>{
         let mainFolder = path.dirname(require.main.filename)
