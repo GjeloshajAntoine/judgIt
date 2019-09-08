fef6f702d1964db9f707128deba8c0067deec8ef
Antoine <gjeloshaj.antoine@gmail.com>
2019-09-08 02:15:34 +0200

    https for express

--- judgit_server/index.js
+++ judgit_server/index.js
@@ -9,6 +9,7 @@ const usersMiddelware = require('./users/middlerware')
 const votes = require('./votes/routes')
 const cookieParser = require('cookie-parser')
 const admin = require('./admin/routes')
+const fs = require('fs');
 
 client.connect().catch(console.log)
 
@@ -19,6 +20,22 @@ app.use(cookieParser())
 app.use('/judgit', admin())
 app.use('/users', users(client))
 app.use('/votes', votes(client, usersMiddelware(client)))
+
+
+if (process.env.node_env == "prod") {
+    const privateKey = fs.readFileSync('/etc/letsencrypt/live/judgit.site/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('/etc/letsencrypt/live/judgit.site/cert.pem', 'utf8');
+    const ca = fs.readFileSync('/etc/letsencrypt/live/judgit.site/chain.pem', 'utf8');
+    const credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: ca
+    };
+    const httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(443, () => {
+        console.log('HTTPS Server running on port 443');
+    }); 
+}
 
 let port = process.env.node_port ? process.env.node_port : 3000
 
